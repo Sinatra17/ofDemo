150c2f8be24d5414fc37fface30d5ffb60eda0e5
Sinatra17 <35964081+Sinatra17@users.noreply.github.com>
2018-02-13 09:17:27 -0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
+#include <iostream>
 #include "ofMain.h"
 #include "ofApp.h"
 
 //========================================================================
+using namespace std;
 int main( ){
 	ofSetupOpenGL(1024,768,OF_WINDOW);			// <-------- setup the GL context
 
@@ -9,5 +11,7 @@ int main( ){
 	// can be OF_WINDOW or OF_FULLSCREEN
 	// pass in width and height too:
 	ofRunApp(new ofApp());
+	cout << "Hello from main";
+	
 
 }
